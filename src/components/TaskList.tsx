db4e4dd15301b13ba1cc4b9bfb9a8429d6a5577c
Elias-Ainsworth <107319227+Elias-Ainsworth@users.noreply.github.com>
2022-06-17 11:57:40 -0300

    Add files via upload

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,48 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return;
+    // A exclamação indica negação, logo, se não houver titulo não prosseguir com a função
+
+    const NewTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    // Dando valor para todos os itens em Task
+    // Math.random() gera um valor aleatório ao ID, não é recomendável utilizar essa função em ID devido a possibilidade de obter o mesmo valor para 2 diferentes tasks
+    // isComplete deve ser falso pois acabamos de criar a Task
+
+    setTasks(oldState => [...oldState, NewTask]);
+    setNewTaskTitle('');
+
+    //com a novateask criada, utilize o setTasks como callback
+    //pegando o oldState, salve o valor dentro de um array (use o spread operator "..." para pegar todos os valores anteriores), e  adicione no final do array o novo valor "NewTask"
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newTasks = tasks.map(task => task.id == id ? {...task, isComplete: !task.isComplete } : task);
+
+    setTasks(newTasks)
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTasks = tasks.filter(task => task.id != id);
+
+    // crie uma task para filtrar todas as tasks que forem diferentes do id que a função recebe
+
+    setTasks(filteredTasks);
+
+    // insira o valor filteredTasks em setTasks
+
   }
 
   return (
