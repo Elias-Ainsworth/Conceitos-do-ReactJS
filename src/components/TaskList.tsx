1c2ff684c7e43faac86e89ee06024d6c8db36871
Lucca Ambrosius <luccaambrosius.wk@gmail.com>
2022-11-20 13:25:27 -0300

    To.DO

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiCheckSquare, FiTrash } from 'react-icons/fi';
 
 interface Task {
   id: number;
@@ -15,10 +15,10 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
 
     if (!newTaskTitle) return;
-    // A exclamação indica negação, logo, se não houver titulo não prosseguir com a função
+    
 
     const NewTask = {
       id: Math.random(),
@@ -26,20 +26,20 @@ export function TaskList() {
       isComplete: false
     }
 
-    // Dando valor para todos os itens em Task
-    // Math.random() gera um valor aleatório ao ID, não é recomendável utilizar essa função em ID devido a possibilidade de obter o mesmo valor para 2 diferentes tasks
-    // isComplete deve ser falso pois acabamos de criar a Task
+    
+    
+    
 
     setTasks(oldState => [...oldState, NewTask]);
     setNewTaskTitle('');
 
-    //com a novateask criada, utilize o setTasks como callback
-    //pegando o oldState, salve o valor dentro de um array (use o spread operator "..." para pegar todos os valores anteriores), e  adicione no final do array o novo valor "NewTask"
+    
+    
 
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
 
     const newTasks = tasks.map(task => task.id == id ? {...task, isComplete: !task.isComplete } : task);
 
@@ -48,15 +48,15 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
 
     const filteredTasks = tasks.filter(task => task.id != id);
 
-    // crie filteredTasks para filtrar todas as tasks que forem diferentes do id que foi recebido a função (uma nova listagem de Tasks)
+    
 
     setTasks(filteredTasks);
 
-    // ponha os valores filtrados em setTasks para a nova exibição em tela.
+    
 
   }
 
